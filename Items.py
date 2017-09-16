a04bdc8abd7fe36d63d39bb01ffcb814eebc41f1
Brandon Suncin <brandon_suncin@outlook.com>
2017-09-16 17:51:34 -0400

    Add files via upload
    Items to be in the game starter file

---
+++ Items.py
@@ -0,0 +1,8 @@
+# Items in the items dictionary will be formatted as follows:
+# key = item name
+# value = tuple of values for the key
+# key : (Value, Physical Attack, Physical Defence, Magic Attack, Magic Defence,
+# Type, Health Recovery, Mana Recovery) 
+
+items = { "HP Potion": (200, 0, 0, 0, 0, "potion", 50, 0)
+          }
