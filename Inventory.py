8a4b203857ff5d4f63951d2d2e7d2e66fc1b7437
Brandon Suncin <29262593+bsuncin@users.noreply.github.com>
2017-09-16 19:19:33 -0400

    Add files via upload
    Unstable version of shop and inventory may change greatly

---
+++ Inventory.py
@@ -0,0 +1,37 @@
+import Item as I
+import Stuff as S
+
+class Inventory:
+
+    def __init__(self):
+        self.gold = 0
+        self.inventory = {}
+
+    def getGold(self):
+        return self.gold
+
+    def addGold(self, num):
+        self.gold += num
+
+    def removeGold(self, num):
+        if num <= self.gold:
+            self.gold -= num
+
+    def addItem(self, item, num):
+        if item in self.inventory:
+            self.inventory[item][1] += num
+        else:
+            self.inventory[item] = (I.Item(item), num)
+
+    def removeItem(self, item, num):
+        if item in self.inventory:
+            if self.inventory[item][1] > num:
+                self.inventory[item][1] -= num
+            elif self.inventory[item][1] == num:
+                del self.inventory[item]
+            else:
+                print("Can't do that.")
+            
+
+    def showInventory(self):
+        
