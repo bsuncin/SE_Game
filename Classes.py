3302aa12c0c2cde141ed6b95c06c33bc83921676
Brandon Suncin <29262593+bsuncin@users.noreply.github.com>
2017-10-05 17:09:21 -0400

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,1 +1,12 @@
 class Class:
+    def __init__(self, attack, speed, xp, lvl, damRes):
+        self.attack = attack
+        self.speed = speed
+        self.xp = xp
+        self.lvl = lvl
+        self.damRes = damRes
+        
+    def __show__(self):
+      print("Attack =", )
+      print("Speed =", )
+    
