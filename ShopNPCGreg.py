af3c434429649c62b58752429b78817c390ee1f0
Thomasz15 <31857100+Thomasz15@users.noreply.github.com>
2017-10-05 18:15:31 -0400

    Update ShopNPCGreg.py

--- ShopNPCGreg.py
+++ ShopNPCGreg.py
@@ -5,3 +5,10 @@ NPCStates = {1: "AYYYY WHATS GOOOIN ON BIG GUY? I AINT SEES YOU ROUND HERE YET. 
              3: "JEEZUS MAN YA COVERED IN GOOP! WAIT DOES THAT MEAN YOU DEFEATED THE FLEM SPITTER?! INCREDABLE! HAVE A POTION ON DA HOUSE.",
              4: " EY MAN, LEMME TELL YA SOMETHING! IF YA EVER SEE A GUY NAMED KOBE TELL HIM TO STOP MOOCHIN!... ANYWAYS YOU WANT SOMETHIN?",
              5: ""}
+
+shoptier1 = {'Fang', 'Ring of Health', 'Mana Potion',
+            'Skelly Eye', 'Leather'}
+shoptier2 = {'Fang', 'Ring of Health', 'Ring of Mana', 'Mana Potion', 'Health Potion',
+            'Skelly Eye', 'Leather', 'Chainmail', 'Sword'}
+shoptier3 = {'Fang', 'Ring of Health', 'Ring of Mana', 'Mana Potion', 'Greater Mana Potion', 'Health Potion', 'Greater Health Potion',
+            'Skelly Eye', 'Leather', 'Chainmail', 'Platemail', 'Dagger', 'Sword'}
