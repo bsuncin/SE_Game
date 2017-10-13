26492e7b8cab7ee2910ad6e18ad1474d6cf4b01a
Brandon Suncin <29262593+bsuncin@users.noreply.github.com>
2017-10-13 07:15:53 -0400

    Added Stuff

--- StateTracker.py
+++ StateTracker.py
@@ -1,10 +1,31 @@
 #import Player as P
 #import Inventory as I
 
-worldState = 1
-shopGregState = 1
-arcadeSate = 1
-dungeonState = 1
-innState = 1
+class State:
+    def __init__(self):
+        self.worldState = 1
+        self.arcadeSate = 1
+        self.dungeonState = 1
+    
+        self.worldS = {1 : "Start state",
+                  2 : "Player beat mid dungeon",
+                  3 : "PLayer beat dungeon"}
 
-worldS = {1 : "Start state",}
+        self.arcadeS = {1 : "First game unlocked",
+                   2 : "Second game unlocked",
+                   3 : "Third game unlocked",
+                   4 : "Fourth game unlocked"}
+
+        self.dungeonS = {1 : "First floor entrance",
+                    2 : "Fifth floor save",
+                    3 : "Tenth floor save",
+                    4 : "Fifteenth floor save",
+                    5 : "Twentieth floor save",
+                    6 : "Dungeon maze room save"}
+
+        def increaseWorldS(self):
+            self.worldState += 1
+            
+        def increaseArcadeS(self):
+            self.arcadeSate += 1
+    
