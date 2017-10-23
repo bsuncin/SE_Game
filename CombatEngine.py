5d18b078745e7e71f68a6fb8ef3a4909f5b16fc0
kjerrett <31856903+kjerrett@users.noreply.github.com>
2017-10-23 10:20:42 -0400

    Update CombatEngine.py

--- CombatEngine.py
+++ CombatEngine.py
@@ -4,7 +4,9 @@ import queue
 
 class Combat:
     def __init__(self, player, enemy):
-        
+    
+    # Returns how much damage a single weapon owned by the player will 
+    # do to an enemy 
     def calcWeaponDam(Pweapon, enemy):
         if Pweapon.dtype == "magic"
             return int(player.wiz + (Pweapon.damage * (enemy.mres / 100)))
@@ -12,7 +14,8 @@ class Combat:
             return int((player.strength / 2) + (Pweapon.damage * (enemy.armor / 100)))
         elif Pweapon.dtype == "physical" and Pweapon.hands == 2
             return int(player.strength + (Pweapon.damage * (enemy.armor / 100)))
-        
+    
+    # Returns the total damage the player will do to and enemy
     def calcPlayerDamage(player, enemy):
         if player.firsthand[0] == True and player.secondhand[0] == True
             hand1 = calcWeaponDam(player.firsthand[1], enemy)
@@ -21,10 +24,12 @@ class Combat:
         elif player.firsthand[0] == True
             return calcWeaponDam(player.firsthand[1], enemy)
         else return int(player.strength / 4)
-
+    
+    # Returns how much damage an enemy will do to the player
     def calcEnemyDamage(player, enemy):
         return (enemy.damage * (player.armor/100))
    
+    # Defines what will happen when player hp = 0 during combat
     def loseCase(player)
         player.gold = int(player.gold * .9)
         invn = player.inventory.getInventory()
@@ -32,6 +37,7 @@ class Combat:
         player.inventory.removeitem(item[0], 1)
         player.inventory.removeitem(item[0], 1)
 
+    # Defines what will happen when enemy hp = 0 during combat 
     def winCase(player, enemy)
         player.addgold(player, enemy.gold)
         player.xp += enemy.xp
