ccd8a88a7d3d97c0f871fc5c167f0549a442934e
Light302 <31856849+Light302@users.noreply.github.com>
2017-11-29 10:34:03 -0500

    Update Skills.py

--- Skills.py
+++ Skills.py
@@ -172,7 +172,7 @@ class Headbutt(foodmachinebroke):
         super().__init__(name="Headbutt",
                          value=0,
                          desc="Nobody wins in a headbutt.",
-                         hcost=20,
+                         hcost=10,
                          mcost=0,
                          damage=20,
                          dtype="physcial",
